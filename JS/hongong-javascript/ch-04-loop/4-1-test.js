afdfd5688dcc9521f326a029be4ad2018513faed
Eastyn <eastynwork@gmail.com>
2025-01-22 22:21:50 +0900

    feat($hongong-js) - make an environment for the example solutions (04-1.배열)

---
+++ JS/hongong-javascript/ch-04-loop/4-1-test.js
@@ -0,0 +1,9 @@
+const todos = ['우유 구매', '업무 매일 확인하기', '필라테스 수업'];
+console.log(todos);
+
+todos.push('저녁 식사 준비하기');
+console.log(todos);
+
+const fruitsA = ['사과', '배', '바나나'];
+fruitsA[10] = '귤';
+console.log(fruitsA);
